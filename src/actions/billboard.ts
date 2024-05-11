ff261f90983a2ab9865b289d9e59dde9e31db943
Coders-Vision <fowzan.alpha@gmail.com>
2024-05-12 02:34:45 +0300

    feat. Added fetch based instance for server actions

--- src/actions/billboard.ts
+++ src/actions/billboard.ts
@@ -1,9 +1,18 @@
 "use server";
 
+// import { serverInstance } from "@/lib/axios";
+import { serverInstance } from "@/lib/fetch-instance";
+import { ResponseSuccess } from "@/types/api-response";
 import { Billboard } from "@/types/types";
-import { serverInstance } from "@/lib/axios";
+
+// export const getBillboards = async (): Promise<Billboard[]> => {
+//   const response = await serverInstance.get(`/catalogue/client/billboards`);
+//   return response.data.data;
+// };
 
 export const getBillboards = async (): Promise<Billboard[]> => {
-  const response = await serverInstance.get(`/catalogue/client/billboards`);
-  return response.data.data;
+  const response = await serverInstance<ResponseSuccess<Billboard[]>>(
+    `/catalogue/client/billboards`
+  );
+  return response.data;
 };
--- src/actions/category.ts
+++ src/actions/category.ts
@@ -1,15 +1,30 @@
 "use server";
 
-import { serverInstance } from "@/lib/axios";
+// import { serverInstance } from "@/lib/axios";
+import { serverInstance } from "@/lib/fetch-instance";
+import { ResponseSuccess } from "@/types/api-response";
 import { Category } from "@/types/types";
 
+// export const getCategories = async (): Promise<Category[]> => {
+//   const response = await serverInstance.get(`/catalogue/client/categories`);
+//   return response.data.data;
+// };
+
+// export const getCategory = async (categoryId: string): Promise<Category> => {
+//   const response = await serverInstance.get(`/catalogue/client/categories/${categoryId}`);
+//   return response.data.data;
+// };
 
 export const getCategories = async (): Promise<Category[]> => {
-  const response = await serverInstance.get(`/catalogue/client/categories`);
-  return response.data.data;
+  const response = await serverInstance<ResponseSuccess<Category[]>>(
+    `/catalogue/client/categories`
+  );
+  return response.data;
 };
 
 export const getCategory = async (categoryId: string): Promise<Category> => {
-  const response = await serverInstance.get(`/catalogue/client/categories/${categoryId}`);
-  return response.data.data;
+  const response = await serverInstance<ResponseSuccess<Category>>(
+    `/catalogue/client/categories/${categoryId}`
+  );
+  return response.data;
 };
